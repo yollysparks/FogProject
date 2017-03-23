2eafa74aa0fc157f7a86fcea8b425a2d03b9bd73
mre44 <mre44@10.50.130.156>
2017-03-23 11:10:25 +0100

    a test3

--- ProjectFog/src/java/Model/User.java
+++ ProjectFog/src/java/Model/User.java
@@ -35,7 +35,7 @@ public class User {
 
     public String getPassword() {
         return password;
-       // whatsuosfgfdgf
+       // whatsuosfgfdgfdfghgf
     }
 
     public void setPassword(String password) {
